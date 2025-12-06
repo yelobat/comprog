fbe7340523fdfa4f73bb955ea82740fbe2759c50
yelobat <lukeanthonyholland@gmail.com>
2025-12-06 20:28:12 +0000

    updated main.rs with more generic parsing logic.

--- crates/solve/src/main.rs
+++ crates/solve/src/main.rs
@@ -1,77 +1,75 @@
-//! My submission to A. Sleeping Through Classes
+//! My submission to A. Little Fairy's Painting
 //!
 //! This contains some Rust boilerplate for parsing the input
 //! of the problem as well as common data structures. It is
 //! currently a work in progress and will be iterated upon
 //! as I solve more and more problems.
 
-use std::io;
+use std::collections::HashSet;
+use std::{io, str::FromStr};
+use std::fmt::Debug;
 
-/// This is just a simple u32 type read from stdin.
-fn read_u32() -> u32 {
-    let mut line = String::new();
-    io::stdin().read_line(&mut line).expect("Failed to read a line");
-    line.trim().parse().unwrap()
-}
-
-/// This is white-space separated list of items.
+/// Parses generic whitespace separated list of types.
 /// # Example
-/// 4 1 2 -> vec![4, 1, 2]
-///
-/// It would be nice to generalise this to parse an
-/// whitespace separated list of things.
-fn read_vec_u32() -> Vec<u32> {
+/// 4 1 2   -> vec![4, 1, 2]
+/// 4 -8 -1 -> vec![4, -8, -1]
+fn read_vec<T>() -> Vec<T>
+where
+    T: FromStr, <T as FromStr>::Err: Debug,
+{
     let mut line = String::new();
     io::stdin().read_line(&mut line).expect("Failed to read a line");
     line
-        .split(" ")
-        .filter(|&s| s != " ")
-        .map(|s| s.trim().parse().unwrap())
+        .split_whitespace()
+        .map(|s| s.trim().parse::<T>().unwrap())
         .collect()
 }
 
-/// This is just a simple String type read from stdin.
-fn read_string() -> String {
+/// Parses a single line containing the type T.
+fn read<T>() -> T
+where
+    T: FromStr, <T as FromStr>::Err: Debug,
+{
     let mut line = String::new();
     io::stdin().read_line(&mut line).expect("Failed to read a line");
-    line.trim().to_string()
+    line.trim().parse::<T>().unwrap()
 }
 
-fn solve(_n: u32, k: u32, s: String) -> u32 {
-    let mut ans: u32 = 0;
-    let mut allowed: u32 = 0;
-    for c in s.chars() {
-        if c == '1' {
-            allowed = k;
-        } else {
-            if allowed == 0 {
-                ans += 1;
-            } else {
-                allowed -= 1;
-            }
-        }
+/// Implement the solution here.
+fn solve(_n: u64, a: Vec<u64>) -> u64 {
+    let mut s = HashSet::new();
+    let mut chosen = a[0];
+    for &v in a.iter() {
+        s.insert(v);
+        chosen = chosen.max(v);
     }
 
-    ans
+    let l = s.len() as u64;
+    for v in a.iter() {
+        if v >= &l && v <= &chosen {
+            chosen = *v;
+        }
+    }
+    chosen
 }
 
+/// Parse the input and pass input into `solve`.
 fn main() {
-    let t: u32 = read_u32();
+    let t: u32 = read::<u32>();
 
     for _ in 0..t {
-        let v = read_vec_u32();
-        let n: u32 = v[0];
-        let k: u32 = v[1];
-        let s: String = read_string();
-        println!("{}", solve(n, k, s));
+        let n = read::<u64>();
+        let a = read_vec::<u64>();
+        println!("{}", solve(n, a));
     }
 }
 
 /// Write the test cases below here
 #[test]
 fn sample_input() {
-    assert_eq!(solve(4, 1, "1001".into()), 1);
-    assert_eq!(solve(3, 3, "000".into()), 3);
-    assert_eq!(solve(3, 1, "001".into()), 2);
-    assert_eq!(solve(8, 2, "01000101".into()), 2);
+    assert_eq!(solve(6, vec![1, 1, 1, 1, 1, 1]), 1);
+    assert_eq!(solve(1, vec![1000]), 1000);
+    assert_eq!(solve(5, vec![8, 10, 15, 20, 25]), 8);
+    assert_eq!(solve(8, vec![2, 5, 2, 4, 1, 2, 5, 3]), 5);
+    assert_eq!(solve(6, vec![40, 4, 1, 95, 8, 40]), 8);
 }
